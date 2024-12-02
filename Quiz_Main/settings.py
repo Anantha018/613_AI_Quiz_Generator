82a60d96a266424e8743f84cacefb6aded9abcdd
sridhar anantha <vedsri1234@gmail.com>
2024-12-02 12:10:15 -0500

    some icons and spacing stuff

--- Quiz_Main/settings.py
+++ Quiz_Main/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-v&i-zg4&q7$29rm9g-nqk@9@ky=&2kw8duau8^j$*xtidq5cax
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Make false when running in prod
-DEBUG = False 
+DEBUG = True 
 
 ALLOWED_HOSTS = ['*']
 
